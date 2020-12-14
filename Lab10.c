f3689d1e301b70db16f555c5bcc4865c5f5099b3
StephenMester <72656327+StephenMester@users.noreply.github.com>
2020-12-14 15:04:41 +0000

    Add files via upload
    Use of pointers

--- Lab10.c
+++ Lab10.c
@@ -2,21 +2,25 @@
 
 #define SIZE 3
 
+//program to copy array into array 2 using only pointer notation
+
 int main()
 {
-    
-    float array [SIZE] = {1.1, 2.2, 3.3};
+
+    float array [SIZE];
     float array2 [SIZE];
-    float *ptr;
     int i;
+    
+    for (i = 0; i < SIZE; i++)
+    {
+        scanf("%f", (array + i));
+        *(array2 + i) = *(array + i);
+    }
 
     for (i = 0; i < SIZE; i++)
     {
-        *(array2 + i) = *(array + i);
-        printf("%f == %f", *(array2 + i), *(array + i));
-        printf("\n");
+        printf("%.2f == %.2f\n", *(array + i), *(array2 + i));
     }
-
 
 
     return 0;
